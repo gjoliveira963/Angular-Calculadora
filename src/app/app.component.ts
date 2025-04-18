3ca28c2e3fe08e712e18ba6f70e7509cf310e6ad
G. J. Oliveira - Web Dev <gjoliveira.dev@outlook.com>
2025-04-18 14:53:46 -0300

    feat: reorganizar estrutura do projeto Angular e adicionar componente de calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { CalculadoraComponent } from "./calculadora/calculadora.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, CalculadoraComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'angular';
+	title = "angular";
 }
